24a065ff47012bd3eccd44fd51bf5c92775b181b
Nicolas Pavie <nicolas.pavie@braillenet.org>
2021-12-14 10:30:32 +0100

    [Conversion library] Pipeline 2 integration and scripts chaining
    - new Pipeline 2 runner and scripts base class
    - new Daisy3ToEPUB3 pipeline 2 script connector
    - new DtbookToEpub3 scripts chain
    - Updated ConverterHelper to provide pipeline 2 infos
    
    - Including the pipeline 2 lite/bridged version
    with both 32 and 64bit jre
    - Including jnet library

---
+++ Common/DaisyConverterLib/Pipeline/ChainedScripts/DtbookToEpub3.cs
@@ -0,0 +1,85 @@
+﻿using Daisy.SaveAsDAISY.Conversion.Pipeline.Pipeline2.Scripts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Daisy.SaveAsDAISY.Conversion.Pipeline.ChainedScripts {
+    public class DtbookToEpub3 : Script {
+
+        Script dtbookToDaisy;
+        Script DaisyToEpub3;
+
+        public DtbookToEpub3() {
+            dtbookToDaisy = Pipeline1.Instance.getScript("default");
+            DaisyToEpub3 = new Daisy3ToEPUB3();
+            // TODO : for now we consider the 3 global steps of the progression but some granularity within
+            // scripts could be taked in account
+            StepsCount = 4;
+            // use default script parameters to not change the form for now
+            this._parameters = dtbookToDaisy.Parameters;
+        }
+
+        public override void ExecuteScript(string inputPath, bool isQuite) {
+
+            // Create a directory using the document name
+            string finalOutput = Path.Combine(
+                Parameters["outputPath"].ParameterValue,
+                Path.GetFileNameWithoutExtension(inputPath)
+                ) ;
+            // Remove and recreate result folder
+            // Since the DaisyToEpub3 requires output folder to be empty
+            if (Directory.Exists(finalOutput)) {
+                Directory.Delete(finalOutput, true);
+            }
+            Directory.CreateDirectory(finalOutput);
+
+            DirectoryInfo tempDir = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()));
+
+            if (this.OnPipelineProgress != null) {
+                OnPipelineProgress("Converting dtbook to DAISY3");
+                dtbookToDaisy.setPipelineProgressListener(OnPipelineProgress);
+            }
+            if (this.OnPipelineOutput != null) {
+                dtbookToDaisy.setPipelineOutputListener(OnPipelineOutput);
+            }
+            if (OnPipelineError != null) {
+                dtbookToDaisy.setPipelineErrorListener(OnPipelineError);
+            }
+            dtbookToDaisy.Parameters["outputPath"].ParameterValue = tempDir.FullName;
+            dtbookToDaisy.ExecuteScript(inputPath,true);
+
+            
+            DaisyToEpub3.Parameters["input"].ParameterValue = Directory.GetFiles(tempDir.FullName, "*.opf")[0];
+            
+            DaisyToEpub3.Parameters["output"].ParameterValue = finalOutput;
+            if (this.OnPipelineProgress != null) {
+                OnPipelineProgress("Converting DAISY3 to EPUB3");
+                DaisyToEpub3.setPipelineProgressListener(OnPipelineProgress);
+            }
+            if (this.OnPipelineOutput != null) {
+                DaisyToEpub3.setPipelineOutputListener(OnPipelineOutput);
+            }
+            if(OnPipelineError != null) {
+                DaisyToEpub3.setPipelineErrorListener(OnPipelineError);
+            }
+            DaisyToEpub3.ExecuteScript(DaisyToEpub3.Parameters["input"].ParameterValue);
+
+            if (File.Exists(Path.Combine(finalOutput, "result.epub"))) {
+                if (this.OnPipelineProgress != null) {
+                    OnPipelineProgress("Moving result.epub to " + Path.Combine(finalOutput, Path.GetFileNameWithoutExtension(inputPath) + ".epub"));
+                    //DaisyToEpub3.setPipelineProgressListener(OnPipelineProgress);
+                }
+                File.Move(
+                    Path.Combine(finalOutput, "result.epub"),
+                    Path.Combine(finalOutput, Path.GetFileNameWithoutExtension(inputPath) + ".epub")
+                );
+                if (this.OnPipelineProgress != null) {
+                    OnPipelineProgress("Successfully converted the document to " + Path.Combine(finalOutput, Path.GetFileNameWithoutExtension(inputPath) + ".epub"));
+                    //DaisyToEpub3.setPipelineProgressListener(OnPipelineProgress);
+                }
+            }
+        }
+    }
+}
---
+++ Common/DaisyConverterLib/Pipeline/Pipeline2/Pipeline2Script.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using org.daisy.jnet;
+
+namespace Daisy.SaveAsDAISY.Conversion {
+
+    /// <summary>
+    /// Abstract class for pipeline 2 scripts launch
+    /// </summary>
+    public abstract class Pipeline2Script : Script {
+
+        //IntPtr currentJob = IntPtr.Zero;
+
+        /// <summary>
+        /// executes script using jnet bridge
+        /// </summary>
+        /// <param name="inputPath">not used for pipeline 2 script : input is a script parameter</param>
+        /// <param name="isQuite"></param>
+        public override void ExecuteScript(string inputPath, bool isQuite) {
+
+            Pipeline2 pipeline = Pipeline2.Instance;
+            // convert parameters list to options dictionnary
+            if (this.OnPipelineError != null) {
+                Pipeline2.Instance.SetPipelineErrorListener((message) => {
+                    this.OnPipelineError(message);
+                });
+            }
+            if (this.OnPipelineOutput != null) {
+                Pipeline2.Instance.SetPipelineOutputListener((message) => {
+                    this.OnPipelineOutput(message);
+                });
+            }
+
+            IntPtr currentJob = Pipeline2.Instance.Start(
+                Name,
+                Parameters.ToDictionary(keyvalue => keyvalue.Value.Name, keyvalue => keyvalue.Value.ParameterValue)
+            );
+
+
+
+            if (currentJob != IntPtr.Zero) {
+                IntPtr context = pipeline.getContext(currentJob);
+                IntPtr monitor = pipeline.getMonitor(context);
+                IntPtr messages = pipeline.getMessageAccessor(monitor);
+                bool checkStatus = true;
+                while (checkStatus) {
+                    if(this.OnPipelineOutput != null) {
+                        foreach (string message in pipeline.getLastMessages(messages)) {
+                            this.OnPipelineOutput(message);
+                        }
+                        
+                    }
+                    //Console.WriteLine(pipeline.getProgress(messages));
+                    
+                    //Console.WriteLine("checking status");
+                    switch (pipeline.getStatus(currentJob)) {
+                        case Pipeline2.JobStatus.IDLE:
+                            break;
+                        case Pipeline2.JobStatus.RUNNING:
+                            break;
+                        case Pipeline2.JobStatus.SUCCESS:
+                            checkStatus = false;
+                            break;
+                        case Pipeline2.JobStatus.ERROR:
+                            checkStatus = false;
+                            break;
+                        case Pipeline2.JobStatus.FAIL:
+                            checkStatus = false;
+                            break;
+                        default:
+                            break;
+                    }
+                    System.Threading.Thread.Sleep(1000);
+#if DEBUG
+                // Kill the instance and running jvm for pipeline debugging
+                try {
+                    //Pipeline2.KillInstance();
+                } catch (Exception e) {
+                    if (this.OnPipelineError != null) {
+                        this.OnPipelineError(e.Message);
+                    }
+                }
+                //
+#else
+                    if (!isQuite && !string.IsNullOrEmpty(output))
+                        System.Diagnostics.Process.Start(output);
+#endif
+                }
+            } else {
+                throw new Exception("An unknown error occured while launching the script " + this.Name + " with the parameters " +
+                    this.Parameters.Aggregate(
+                        "",
+                        (result, keyvalue) => result + keyvalue.Value.Name + "=" + keyvalue.Value.ParameterValue.ToString() +"\r\n"
+                    )
+                );
+            }
+
+        }
+    }
+}
