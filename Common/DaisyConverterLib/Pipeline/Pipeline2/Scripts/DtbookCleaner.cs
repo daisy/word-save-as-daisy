755b397a407c25a1eac13a27ddee5b493613ff55
Nicolas Pavie <pavie.nicolas@gmail.com>
2024-10-21 16:43:26 +0200

    chore: update localization project

--- Common/DaisyConverterLib/Pipeline/Pipeline2/Scripts/DtbookCleaner.cs
+++ Common/DaisyConverterLib/Pipeline/Pipeline2/Scripts/DtbookCleaner.cs
@@ -172,47 +172,6 @@ namespace Daisy.SaveAsDAISY.Conversion.Pipeline.Pipeline2.Scripts
             };
         }
 
-        public override void ExecuteScript(string inputPath, bool isQuite)
-        {
-            // Create a directory using the document name
-            string finalOutput = Path.Combine(
-                Parameters["output"].ParameterValue.ToString(),
-                string.Format(
-                    "{0}_DtbookXML_{1}",
-                    Path.GetFileNameWithoutExtension(inputPath),
-                    DateTime.Now.ToString("yyyyMMddHHmmssffff")
-                )
-            );
-            // Remove and recreate result folder
-            if (Directory.Exists(finalOutput))
-            {
-                Directory.Delete(finalOutput, true);
-            }
-            Directory.CreateDirectory(finalOutput);
-            // Update final output with the new subdirectory
-            Parameters["output"].ParameterValue = finalOutput;
-            // Execute the script
-            base.ExecuteScript(inputPath, isQuite);
-            // TODO : fix the resource copy in pipeline 2 script later :
-            string scriptInput = this._parameters["input"].ParameterValue.ToString();
-            string currentDirectory = Path.GetDirectoryName(scriptInput);
-            string currentFile = Path.GetFileName(scriptInput);
-            string outputDir = this._parameters["output"].ParameterValue.ToString();
-
-            string[] files = Directory.GetFiles(currentDirectory, "*", SearchOption.AllDirectories);
-            foreach (string file in files)
-            {
-                if(Path.GetFileName(file) != currentFile)
-                {
-                    string outputFile = Path.Combine(
-                        outputDir,
-                        Path.GetFileName(file)
-                    );
-                    File.Copy(file,outputFile,true);
-                }
-            }
-            
-        }
 
         /// <summary>
         /// 
