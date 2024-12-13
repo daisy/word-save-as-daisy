bc3fb29887b5e66ca66a15207eb58a529d1c5e99
Nicolas Pavie <pavie.nicolas@gmail.com>
2024-12-13 11:35:44 +0100

    feat(engine): using the new app connection for conversion
    For Pipeline 2 Script, added the execution of a script through the DAISY Pipeline App embedded engine webservice.
    Results of the execution are retrieved using the job  zipped results uri and unzipped in the script ouput target
    
    If the app is not installed or a crash happens, the script execution falls back on the addin embedded engine

--- Common/DaisyConverterLib/Pipeline/Pipeline2/Pipeline2Script.cs
+++ Common/DaisyConverterLib/Pipeline/Pipeline2/Pipeline2Script.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
-using System.Text;
 using Daisy.SaveAsDAISY.Conversion.Events;
-using Daisy.SaveAsDAISY.Conversion.Pipeline.Pipeline2.Scripts;
-using org.daisy.jnet;
 
 namespace Daisy.SaveAsDAISY.Conversion
 {
@@ -16,14 +14,15 @@ namespace Daisy.SaveAsDAISY.Conversion
     {
         public class JobException : Exception
         {
-            public JobException(string message) : base(message)
-            {
-            }
+            public JobException(string message)
+                : base(message) { }
         }
+
         protected Pipeline2Script(IConversionEventsHandler e)
             : base(e) { }
 
         public abstract string searchInputFromDirectory(DirectoryInfo inputDirectory);
+
         //IntPtr currentJob = IntPtr.Zero;
 
         /// <summary>
@@ -33,11 +32,41 @@ namespace Daisy.SaveAsDAISY.Conversion
         /// <param name="isQuite"></param>
         public override void ExecuteScript(string inputPath, bool isQuite)
         {
+            try
+            {
+                TryExecuteOnAppWebservice(inputPath, isQuite);
+            }
+            catch (Exception e)
+            {
+                this.EventsHandler.onFeedbackMessageReceived(
+                    this,
+                    new DaisyEventArgs(
+                        $"The script could not be launched on Pipeline App webservice:{e.Message}\r\n > Using addin embedded pipeline"
+                    )
+                );
+                ExecuteOnEmbeddedPipeline(inputPath, isQuite);
+                //throw new Exception("An error occured while executing the script " + this.Name, e);
+            }
+        }
+
+        /// <summary>
+        /// Run the pipeline 2 script on an embedded pipeline
+        /// </summary>
+        /// <param name="inputPath"></param>
+        /// <param name="isQuite"></param>
+        /// <exception cref="JobException"></exception>
+        /// <exception cref="Exception"></exception>
+        public void ExecuteOnEmbeddedPipeline(string inputPath, bool isQuite)
+        {
             Pipeline2 pipeline = Pipeline2.Instance;
             Pipeline2.Instance.SetPipelineErrorListener(
                 (message) =>
                 {
-                    this.EventsHandler.OnConversionError(new Exception("Pipeline 2 returned the following error message :\r\n" + message));
+                    this.EventsHandler.OnConversionError(
+                        new Exception(
+                            "Pipeline 2 returned the following error message :\r\n" + message
+                        )
+                    );
                 }
             );
             Pipeline2.Instance.SetPipelineOutputListener(
@@ -82,9 +111,6 @@ namespace Daisy.SaveAsDAISY.Conversion
                             new DaisyEventArgs("DP2 > " + message)
                         );
                     }
-                    //Console.WriteLine(pipeline.getProgress(messages));
-                    // TODO need to get jobs log
-                    //Console.WriteLine("checking status");
                     switch (pipeline.getStatus(currentJob))
                     {
                         case Pipeline2.JobStatus.IDLE:
@@ -148,6 +174,193 @@ namespace Daisy.SaveAsDAISY.Conversion
                                 + "\r\n"
                         )
                 );
+            }
+        }
+
+        public void TryExecuteOnAppWebservice(string inputPath, bool isQuite)
+        {
+            Webservice engine = null;
+            if (!PipelineApp.IsInstalled())
+            {
+                throw new Exception("DAISY pipeline app is not installed");
+            }
+            if (!PipelineApp.IsRunning())
+            {
+                // try launching daisy pipeline app in background
+                // wait for it to start
+                engine = PipelineApp.Start();
+            }
+            else
+            {
+                engine = PipelineApp.FindWebservice();
+            }
+            if (engine == null)
+            {
+                throw new Exception("Could not access the DAISY pipeline app webservice");
+            }
+            // Check if pipeline is alive
+            Alive alive = engine.FetchAlive();
+            if (!alive.alive)
+            {
+                throw new Exception("DAISY pipeline app webservice is not responding");
+            }
+            if (Parameters.ContainsKey("input") && (string)Parameters["input"].ParameterValue == "")
+            {
+                Parameters["input"].ParameterValue = inputPath;
+            }
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            foreach (KeyValuePair<string, ScriptParameter> v in Parameters)
+            {
+                // avoid passing empty values
+                if (
+                    !v.Value.IsParameterRequired
+                    && (
+                        v.Value.ParameterDataType is StringDataType
+                        || v.Value.ParameterDataType is PathDataType
+                    )
+                    && "" == (string)v.Value.ParameterValue
+                )
+                {
+                    continue;
+                }
+                parameters[v.Value.Name] = v.Value.ParameterValue;
+            }
+            string outputFolder = Path.GetDirectoryName(
+                Parameters["output"].ParameterValue.ToString()
+            );
+            JobRequest jobStart = JobRequest.fromScript(engine, this);
+            JobData job = null;
+            bool checkStatus = true;
+            List<string> errors;
+            while (checkStatus)
+            {
+                job = job == null ? engine.LaunchJob(jobStart) : engine.FetchJobDetails(job);
+                this.EventsHandler.onFeedbackMessageReceived(
+                    this,
+                    new DaisyEventArgs("App > " + job.Messages.ToString())
+                );
+                switch (job.Status.ToLower())
+                {
+                    case "success":
+                        checkStatus = false;
+                        if (!Directory.Exists(outputFolder))
+                        {
+                            Directory.CreateDirectory(outputFolder);
+                        }
+                        // Download the result into the selected output folder
+                        byte[] result = engine.FetchResults(job);
+                        if (job.Results.MimeType == "application/zip")
+                        {
+                            UnzipByteArray(result, Parameters["output"].ParameterValue.ToString());
+                        }
+                        else
+                        {
+                            // TODO: should not be necessary (i'm expected a zip on this call)
+                            // but need to check for mimetype and set the correct extension
+                            File.WriteAllBytes(Path.Combine(outputFolder, "result"), result);
+                        }
+                        File.WriteAllText(
+                            Path.Combine(outputFolder, "success.log"),
+                            engine.FetchJobLog(job)
+                        );
+                        engine.DeleteJob(job);
+                        break;
+                    case "error":
+                        string errorMessage =
+                            " DP2 > "
+                            + this.NiceName
+                            + "conversion job has finished in error :\r\n"
+                            + "Please check the log stored here:\r\n"
+                            + Path.Combine(outputFolder, "error.log");
+                        if (!Directory.Exists(outputFolder))
+                        {
+                            Directory.CreateDirectory(outputFolder);
+                        }
+                        File.WriteAllText(
+                            Path.Combine(outputFolder, "error.log"),
+                            engine.FetchJobLog(job)
+                        );
+                        engine.DeleteJob(job);
+                        throw new JobException(errorMessage);
+                    case "fail":
+                        // open jobs folder
+                        string failedMessage =
+                            " DP2 > "
+                            + this.NiceName
+                            + " conversion job failed :\r\n"
+                            + "Please check the log stored here:\r\n"
+                            + Path.Combine(outputFolder, "error.log");
+                        if (!Directory.Exists(outputFolder))
+                        {
+                            Directory.CreateDirectory(outputFolder);
+                        }
+                        File.WriteAllText(
+                            Path.Combine(outputFolder, "error.log"),
+                            engine.FetchJobLog(job)
+                        );
+                        engine.DeleteJob(job);
+                        throw new JobException(failedMessage);
+                    case "idle":
+                    case "running":
+                    default:
+                        break;
+                }
+                System.Threading.Thread.Sleep(1000);
+
+#if DEBUG
+                // Kill the instance and running jvm for pipeline debugging
+                try
+                {
+                    //Pipeline2.KillInstance();
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+                //
+#else
+                if (!isQuite && !string.IsNullOrEmpty(output))
+                    System.Diagnostics.Process.Start(output);
+#endif
+            }
+        }
+
+        public static void UnzipByteArray(byte[] zipBytes, string toBasePath)
+        {
+            if (!Directory.Exists(toBasePath))
+            {
+                Directory.CreateDirectory(toBasePath);
+            }
+            using (var memoryStream = new MemoryStream(zipBytes))
+            {
+                using (var zipArchive = new ZipArchive(memoryStream, ZipArchiveMode.Read))
+                {
+                    foreach (var entry in zipArchive.Entries)
+                    {
+                        using (var entryStream = entry.Open())
+                        {
+                            byte[] buffer = new byte[entry.Length];
+                            entryStream.Read(buffer, 0, buffer.Length);
+                            string relativePath = entry.FullName.StartsWith("result/")
+                                ? entry.FullName.Substring("result/".Length)
+                                : entry.FullName;
+                            relativePath = relativePath.Replace("/", "\\");
+                            string destination = Path.Combine(toBasePath, relativePath);
+                            if (relativePath.EndsWith("/"))
+                            {
+                                Directory.CreateDirectory(destination);
+                            }
+                            else
+                            {
+                                if (!Directory.Exists(Path.GetDirectoryName(destination)))
+                                {
+                                    Directory.CreateDirectory(Path.GetDirectoryName(destination));
+                                }
+                                File.WriteAllBytes(destination, buffer);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
