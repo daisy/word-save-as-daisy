1ea9c9b53ff9deccef9c7fcb5b319f5d4c81bdcb
Nicolas Pavie <pavie.nicolas@gmail.com>
2024-12-13 13:56:43 +0100

    feat(tests): new tests for pipeline app and webservice
    - Replaced the old Regression tests project by a new test focused on fuctionnalities of the conversion library
    - New test to check the PipelineApp and Webservice behavior

---
+++ Tests/ConverterLibTests/WebserviceTests.cs
@@ -0,0 +1,97 @@
+﻿using Daisy.SaveAsDAISY.Conversion;
+using Daisy.SaveAsDAISY.Conversion.Events;
+using Daisy.SaveAsDAISY.Conversion.Pipeline.ChainedScripts;
+using Daisy.SaveAsDAISY.Conversion.Pipeline.Pipeline2.Scripts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.IO;
+
+namespace ConverterLibTests
+{
+    [TestClass]
+    public class WebserviceTests
+    {
+
+        
+        [TestMethod]
+        public void AppIsInstalled()
+        {
+            Assert.IsTrue(PipelineApp.IsInstalled());
+        }
+
+        [TestMethod]
+        public void AppStart()
+        {
+            if(!PipelineApp.IsRunning())
+            {
+                PipelineApp.Start();
+            }
+            Assert.IsTrue(PipelineApp.IsRunning());
+        }
+
+        [TestMethod]
+        public void EngineIsAlive()
+        {
+            Webservice ws = !PipelineApp.IsRunning() ? PipelineApp.Start() : PipelineApp.FindWebservice();
+            Assert.IsNotNull(ws);
+            Alive testAlive = ws.FetchAlive();
+            Assert.IsNotNull(testAlive);
+        }
+
+        [TestMethod]
+        public void GetScripts()
+        {
+            Webservice test = PipelineApp.FindWebservice();
+            Assert.IsNotNull(test);
+            ScriptData[] testScripts = test.FetchScripts();
+            Assert.IsNotNull(testScripts);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"test-files\\default_sample_for_dtbook_conversion.docx", "test-files")]
+        public void ExecuteScriptOnWebservice()
+        {
+            Assert.IsTrue(File.Exists("test-files\\default_sample_for_dtbook_conversion.docx"));
+            Webservice ws = !PipelineApp.IsRunning() ? PipelineApp.Start() : PipelineApp.FindWebservice();
+            Assert.IsNotNull(ws);
+            Assert.IsTrue(ws.FetchAlive().alive);
+            SilentEventsHandler e = new SilentEventsHandler();
+            WordToDtbook script = new WordToDtbook(e);
+            script.Parameters["input"].ParameterValue = Path.GetFullPath("test-files\\default_sample_for_dtbook_conversion.docx");
+            script.Parameters["output"].ParameterValue = Path.GetFullPath("test-results");
+            if (!Directory.Exists(Path.GetFullPath("test-results"))) {
+                Directory.CreateDirectory(Path.GetFullPath("test-results"));
+            }
+            script.TryExecuteOnAppWebservice(Path.GetFullPath("test-files\\default_sample_for_dtbook_conversion.docx"), false);
+            Assert.IsTrue(File.Exists("test-results\\default_sample_for_dtbook_conversion.xml"));
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"test-files\\default_sample_for_dtbook_conversion.docx", "test-files")]
+        public void ExecuteWordToCleanedDtbookChain()
+        {
+            Assert.IsTrue(File.Exists("test-files\\default_sample_for_dtbook_conversion.docx"));
+            SilentEventsHandler e = new SilentEventsHandler();
+            WordToCleanedDtbook script = new WordToCleanedDtbook(e);
+            script.Parameters["input"].ParameterValue = Path.GetFullPath("test-files\\default_sample_for_dtbook_conversion.docx");
+            script.Parameters["output"].ParameterValue = Path.GetFullPath("test-results");
+            script.ExecuteScript(Path.GetFullPath("test-files\\default_sample_for_dtbook_conversion.docx"), false);
+
+            // Search a file default_sample_for_dtbook_conversion.xml somewhere under the test-results folder
+            DirectoryInfo results = new DirectoryInfo(Path.GetFullPath("test-results"));
+            Assert.IsTrue(results.Exists);
+            Assert.IsTrue(results.GetFiles("default_sample_for_dtbook_conversion.xml", SearchOption.AllDirectories).Any());
+        }
+
+        private static string GetCommandLine(Process process)
+        {
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + process.Id))
+            using (ManagementObjectCollection objects = searcher.Get()) {
+                return objects.Cast<ManagementBaseObject>().SingleOrDefault()?["CommandLine"]?.ToString();
+            }
+
+        }
+    }
+}
