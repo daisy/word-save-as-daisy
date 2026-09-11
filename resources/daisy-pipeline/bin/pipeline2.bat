c5131709a9d61a51e593856846983449b02b687f
Nicolas PAVIE <n.pavie@avh.asso.fr>
2026-09-11 14:06:56 +0200

    feat(engine): updated embedded engine to pipeline 1.15.5
    see pipeline release notes at https://github.com/daisy/pipeline-assembly/blob/master/NEWS.md#changes-in-release-v1155
    
    This version of the pipeline also includes an updated version of the swing progress dialog and an updated Word to dtbook script that reports progress of the word conversion.

---
+++ resources/daisy-pipeline/bin/pipeline2.bat
@@ -0,0 +1,320 @@
+@echo off
+rem
+rem
+rem    Licensed to the Apache Software Foundation (ASF) under one or more
+rem    contributor license agreements.  See the NOTICE file distributed with
+rem    this work for additional information regarding copyright ownership.
+rem    The ASF licenses this file to You under the Apache License, Version 2.0
+rem    (the "License"); you may not use this file except in compliance with
+rem    the License.  You may obtain a copy of the License at
+rem
+rem       http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem    Unless required by applicable law or agreed to in writing, software
+rem    distributed under the License is distributed on an "AS IS" BASIS,
+rem    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem    See the License for the specific language governing permissions and
+rem    limitations under the License.
+rem
+rem    ------------------------------------------------------------------------
+rem
+rem    This script is adapted from the launcher script of the Karaf runtime:
+rem    http://karaf.apache.org/
+rem
+
+if not "%ECHO%" == "" echo %ECHO%
+
+setlocal enabledelayedexpansion
+set DIRNAME=%~dp0
+set PROGNAME=%~nx0
+rem Code to return to launcher on failure
+rem 0:success, 1:unhandled, 2:user-fixable, 3:fatal(we must fix)
+set exitCode=0
+
+
+title Pipeline2
+
+if "%PIPELINE2_DATA%" == "" (
+    set PIPELINE2_DATA=%appdata%/DAISY Pipeline 2
+)
+if not exist "%PIPELINE2_DATA%" mkdir "%PIPELINE2_DATA%"
+
+if "%PIPELINE2_LOGDIR%" == "" (
+    set PIPELINE2_LOGDIR=%appdata%/DAISY Pipeline 2\log
+)
+if not exist "%PIPELINE2_LOGDIR%" mkdir "%PIPELINE2_LOGDIR%"
+
+goto BEGIN
+
+rem # # SUBROUTINES # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+:warn
+    echo %PROGNAME%: %*
+goto :EOF
+
+rem # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+:BEGIN
+    call:warn %DATE% %TIME%
+    REM call:warn %DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2% %TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
+
+    rem # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    if not "%PIPELINE2_HOME%" == "" call:warn Ignoring value for PIPELINE2_HOME
+
+    set PIPELINE2_HOME=%DIRNAME%..
+    if not exist "%PIPELINE2_HOME%" (
+        call:warn PIPELINE2_HOME is not valid: !PIPELINE2_HOME!
+        rem fatal
+        set exitCode=3
+        goto END
+    )
+
+    rem Setup the Java Virtual Machine
+    call "%DIRNAME%\checkJavaVersion.bat" 11
+    if errorLevel 1 (
+        rem Fall back to Java 8 (or 9 or 10) because web server does not work with Java 11
+        call:warn Java 11 not found; Trying Java 8
+        call "%DIRNAME%\checkJavaVersion.bat" 1.8
+        if errorLevel 1 (
+            if errorLevel 2 (
+                rem fatal
+                set exitCode=3
+            ) else (
+                rem user-fixable
+                set exitCode=2
+            )
+            goto END
+        )
+    )
+
+    REM NP - disabling jmxremote by default to avoid a firewall notice
+    REM set DEFAULT_JAVA_OPTS=-Dcom.sun.management.jmxremote
+    REM if "%JAVA_OPTS%" == "" set JAVA_OPTS=%DEFAULT_JAVA_OPTS%
+
+    set DEFAULT_JAVA_DEBUG_OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
+    if "%PIPELINE2_DEBUG%" == "" goto :PIPELINE2_DEBUG_END
+    rem Use the defaults if JAVA_DEBUG_OPTS was not set
+    if "%JAVA_DEBUG_OPTS%" == "" set JAVA_DEBUG_OPTS=%DEFAULT_JAVA_DEBUG_OPTS%
+    set "JAVA_OPTS=%JAVA_DEBUG_OPTS% %JAVA_OPTS%"
+    call:warn Enabling Java debug options: %JAVA_DEBUG_OPTS%
+:PIPELINE2_DEBUG_END
+
+    set ENABLE_PERSISTENCE=true
+    set ENABLE_SHELL=false
+    set MODE=webservice
+:RUN_LOOP
+    if "%~1"=="" goto :EXECUTE
+    REM additionnal parsing : if %1 starts with -D, add it to SYSTEM props
+    set "PARSING=%~1"
+    if "!PARSING:~0,2!" == "-D" goto :PARSE_SYSTEM_PROPS
+    if /I "%~1" == "cli" goto :EXECUTE_CLI
+    if /I "%~1" == "ui" goto :EXECUTE_UI
+    if /I "%~1" == "remote" goto :EXECUTE_REMOTE
+    if /I "%~1" == "local" goto :EXECUTE_LOCAL
+    if /I "%~1" == "clean" goto :EXECUTE_CLEAN
+    if /I "%~1" == "debug" goto :EXECUTE_DEBUG
+    call:warn Unexpected argument: "%1"
+    rem user-fixable
+    set exitCode=2
+    goto END
+goto :EXECUTE
+
+:PARSE_SYSTEM_PROPS
+    set "OPTION=%~1"
+    set "VALUE=%~2"
+    set "SYSTEM_PROPS=%SYSTEM_PROPS% %OPTION%=^"%VALUE%^""
+    shift
+    shift
+goto :RUN_LOOP
+    
+
+:EXECUTE_UI
+    set PIPELINE2_WS_LOCALFS=true
+    set PIPELINE2_WS_AUTHENTICATION=false
+    set ENABLE_PERSISTENCE=false
+    set MODE=ui
+    set ENABLE_SHELL=true
+    shift
+    goto :PARSE_CLI_ARGS
+goto :RUN_LOOP
+
+:EXECUTE_CLI
+    set PIPELINE2_WS_LOCALFS=true
+    set PIPELINE2_WS_AUTHENTICATION=false
+    set ENABLE_PERSISTENCE=false
+    set MODE=cli
+    set ENABLE_SHELL=true
+    shift
+    goto :PARSE_CLI_ARGS
+goto :RUN_LOOP
+
+:PARSE_CLI_ARGS
+    if "%~1"=="" goto :EXECUTE
+    set CLI_ARGS=%CLI_ARGS% "%~1"
+    shift
+    goto :PARSE_CLI_ARGS
+    rem stop parsing argument
+rem goto :EXECUTE
+
+
+:EXECUTE_REMOTE
+    set PIPELINE2_WS_LOCALFS=false
+    set PIPELINE2_WS_AUTHENTICATION=true
+    shift
+goto :RUN_LOOP
+
+
+:EXECUTE_LOCAL
+    set PIPELINE2_WS_LOCALFS=true
+    set PIPELINE2_WS_AUTHENTICATION=false
+    shift
+goto :RUN_LOOP
+
+:EXECUTE_CLEAN
+    rmdir /S /Q "%PIPELINE2_DATA%"
+    shift
+goto :RUN_LOOP
+
+:EXECUTE_DEBUG
+    if "%JAVA_DEBUG_OPTS%" == "" set JAVA_DEBUG_OPTS=%DEFAULT_JAVA_DEBUG_OPTS%
+    set "JAVA_OPTS=%JAVA_DEBUG_OPTS% %JAVA_OPTS%"
+    shift
+goto :RUN_LOOP
+
+:EXECUTE
+    if "%MODE%" == "webservice" (
+        if not exist "%PIPELINE2_HOME%\system\webservice" (
+            rem fatal
+            set exitCode=3
+            goto END
+        ) else (
+            rem set PATHS=!PATHS! system\webservice
+            set PATHS=!PATHS! system\webservice
+            set JAVA_OPTS=-Dcom.sun.management.jmxremote %JAVA_OPTS%
+        )
+    ) else ( rem for cli and ui modes, through the simple-api package provided in the assembly
+        if not exist "%PIPELINE2_HOME%\system\simple-api" (
+            rem fatal
+            set exitCode=3
+            goto END
+        ) else (
+            rem set PATHS=!PATHS! system\webservice
+            rem set PATHS=!PATHS! system\simple-api
+        )
+        
+    )
+    
+    if %ENABLE_PERSISTENCE% == true (
+        if not exist "%PIPELINE2_HOME%\system\persistence" (
+            call:warn Running without persistence
+            set ENABLE_PERSISTENCE=false
+        )
+    )
+    if %ENABLE_PERSISTENCE% == true (
+        set PATHS=!PATHS! system\persistence
+    )
+    
+    for %%D in (system\common !PATHS! modules) do (
+        if exist "%PIPELINE2_HOME%\%%D" (
+            rem Using wildcard to avoid "The input line is too long" error
+            set CLASSPATH=!CLASSPATH!;%%D\*
+            rem for /f %%F in ('dir /b "%PIPELINE2_HOME%\%%D\*.jar"') do (
+            rem     set CLASSPATH=!CLASSPATH!;%%D\%%F
+            rem )
+        )
+    )
+    if "%MODE%" == "webservice" (
+        set MAIN=org.daisy.pipeline.webservice.restlet.impl.PipelineWebService
+    ) else (
+        rem Using runner class from the simple-api unnamed module
+        set CLASSPATH=!CLASSPATH!;system\simple-api\api;system\simple-api\xml;system\simple-api\ui;system\simple-api
+        if "%MODE%" == "ui" (
+            set MAIN=GraphicalInterface %CLI_ARGS%
+        ) else (
+            set MAIN=CommandLineInterface %CLI_ARGS%
+        )
+    )
+    
+    rem Execute the Java Virtual Machine
+    cd "%PIPELINE2_HOME%"
+
+    rem Logback configuration file
+    set SYSTEM_PROPS=%SYSTEM_PROPS% -Dlogback.configurationFile="file:%PIPELINE2_HOME:\=/%/etc/logback.xml"
+    set SYSTEM_PROPS=%SYSTEM_PROPS% -Djava.util.logging.config.file="%PIPELINE2_HOME:\=/%/etc/logging.properties"
+    rem to make ${org.daisy.pipeline.logdir} available in logback.xml
+    set SYSTEM_PROPS=%SYSTEM_PROPS% -Dorg.daisy.pipeline.logdir="%PIPELINE2_LOGDIR%"
+
+    call "%DIRNAME%\checkJavaVersion.bat" _ :compare_versions %JAVA_VER% 9
+    if %ERRORLEVEL% geq 0 (
+        if errorLevel 3 (
+            rem unexpected error
+            call:warn Failed to compare versions: "%JAVA_VER%" with "9"
+            set exitCode=3
+            goto END
+        )
+        rem at least version 9
+        SET COMMAND="%JAVA%" %JAVA_OPTS% ^
+            --add-opens java.base/java.security=ALL-UNNAMED ^
+            --add-opens java.base/java.net=ALL-UNNAMED ^
+            --add-opens java.base/java.lang=ALL-UNNAMED ^
+            --add-opens java.base/java.util=ALL-UNNAMED ^
+            --add-opens java.naming/javax.naming.spi=ALL-UNNAMED ^
+            --add-opens java.rmi/sun.rmi.transport.tcp=ALL-UNNAMED ^
+            --add-exports=java.base/sun.net.www.protocol.http=ALL-UNNAMED ^
+            --add-exports=java.base/sun.net.www.protocol.https=ALL-UNNAMED ^
+            --add-exports=java.base/sun.net.www.protocol.jar=ALL-UNNAMED ^
+            --add-exports=jdk.xml.dom/org.w3c.dom.html=ALL-UNNAMED ^
+            --add-exports=jdk.naming.rmi/com.sun.jndi.url.rmi=ALL-UNNAMED ^
+            -Dorg.daisy.pipeline.properties="%PIPELINE2_HOME%\etc\pipeline.properties" ^
+            %SYSTEM_PROPS% ^
+            -classpath "%CLASSPATH%" ^
+            %MAIN%
+    ) else (
+        rem version 8
+        SET COMMAND="%JAVA%" %JAVA_OPTS% ^
+            -Dorg.daisy.pipeline.properties="%PIPELINE2_HOME%\etc\pipeline.properties" ^
+            %SYSTEM_PROPS% ^
+            -classpath "%CLASSPATH%" ^
+            %MAIN%
+            rem skipping java.endorsed.dirs and java.ext.dirs because this requires JAVA_HOME which is not always available
+            rem -Djava.endorsed.dirs="%JAVA_HOME%\jre\lib\endorsed;%JAVA_HOME%\lib\endorsed;%PIPELINE2_HOME%\lib\endorsed" ^
+            rem -Djava.ext.dirs="%JAVA_HOME%\jre\lib\ext;%JAVA_HOME%\lib\ext;%PIPELINE2_HOME%\lib\ext" ^
+    )
+	if "%MODE%" == "ui" (
+        REM launch Java Access Bridge for UI
+		REM remove "java.exe" from java var to get the bin folder
+		set "JAVA_BIN_DIR=!JAVA:~0,-9!"
+        if exist "!JAVA_BIN_DIR!jabswitch.exe" (
+            "!JAVA_BIN_DIR!jabswitch.exe" /enable
+        )
+    )
+    
+    REM call:warn Starting java: %COMMAND%
+    if %ENABLE_SHELL% == true (
+        rem endlocal & (
+        rem     set "PIPELINE2_HOME=%PIPELINE2_HOME%"
+        rem     set "PIPELINE2_DATA=%PIPELINE2_DATA%"
+        rem     set "PIPELINE2_LOGDIR=%PIPELINE2_LOGDIR%"
+        rem     set "PIPELINE2_WS_LOCALFS=%PIPELINE2_WS_LOCALFS%"
+        rem     set "PIPELINE2_WS_AUTHENTICATION=%PIPELINE2_WS_AUTHENTICATION%"
+        %COMMAND%
+        rem )
+    ) else (
+        call:warn Output is written to daisy-pipeline-java.log
+        rem endlocal & (
+        rem     set "PIPELINE2_HOME=%PIPELINE2_HOME%"
+        rem     set "PIPELINE2_DATA=%PIPELINE2_DATA%"
+        rem     set "PIPELINE2_LOGDIR=%PIPELINE2_LOGDIR%"
+        rem     set "PIPELINE2_WS_LOCALFS=%PIPELINE2_WS_LOCALFS%"
+        rem     set "PIPELINE2_WS_AUTHENTICATION=%PIPELINE2_WS_AUTHENTICATION%"
+        %COMMAND% > "%PIPELINE2_LOGDIR%\daisy-pipeline-java.log"
+        rem )
+    )
+
+rem # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+:END
+    call:warn Exiting with value %exitCode%
+    if not "%PAUSE%" == "" pause
+    exit /b %exitCode%
--- resources/daisy-pipeline/jre/legal/java.desktop/libpng.md
+++ resources/daisy-pipeline/jre/legal/java.desktop/libpng.md
@@ -1,4 +1,4 @@
-## libpng v1.6.57
+## libpng v1.6.38
 
 ### libpng License
 <pre>
@@ -9,11 +9,11 @@ COPYRIGHT NOTICE, DISCLAIMER, and LICENSE
 PNG Reference Library License version 2
 ---------------------------------------
 
-Copyright (C) 1995-2026 The PNG Reference Library Authors.
-Copyright (C) 2018-2026 Cosmin Truta
-Copyright (C) 1998-2018 Glenn Randers-Pehrson
-Copyright (C) 1996-1997 Andreas Dilger
-Copyright (C) 1995-1996 Guy Eric Schalnat, Group 42, Inc.
+Copyright (c) 1995-2022 The PNG Reference Library Authors.
+Copyright (c) 2018-2022 Cosmin Truta
+Copyright (c) 1998-2018 Glenn Randers-Pehrson
+Copyright (c) 1996-1997 Andreas Dilger
+Copyright (c) 1995-1996 Guy Eric Schalnat, Group 42, Inc.
 
 The software is supplied "as is", without warranty of any kind,
 express or implied, including, without limitation, the warranties
@@ -157,10 +157,7 @@ PNG REFERENCE LIBRARY AUTHORS
 This is the list of PNG Reference Library ("libpng") Contributing
 Authors, for copyright and licensing purposes.
 
- * Adam Richter
- * Alexander Smorkalov
  * Andreas Dilger
- * Chris Blume
  * Cosmin Truta
  * Dave Martindale
  * Eric S. Raymond
@@ -168,64 +165,39 @@ Authors, for copyright and licensing purposes.
  * Glenn Randers-Pehrson
  * Greg Roelofs
  * Guy Eric Schalnat
- * Halil Oktay
  * James Yu
  * John Bowler
- * Joshua Inscoe
  * Kevin Bracey
- * Lucas Chollet
  * Magnus Holmgren
  * Mandar Sahastrabuddhe
- * Manfred Schlaegl
  * Mans Rullgard
  * Matt Sarett
  * Mike Klein
- * Mohammad Seet
  * Pascal Massimino
  * Paul Schmidt
- * Petr Simecek
- * Philippe Antoine
  * Qiang Zhou
  * Sam Bushell
  * Samuel Williams
  * Simon-Pierre Cadieux
- * Taegu Ha (하태구)
  * Tim Wegner
- * Tobias Stoeckmann
  * Tom Lane
  * Tom Tanner
  * Vadim Barkov
  * Willem van Schaik
- * Yuelin Wang (王跃林)
  * Zhijie Liang
- * Apple Inc.
-    - Zixu Wang (王子旭)
  * Arm Holdings
-    - Richard Townsend
- * Google LLC
-    - Dan Field
-    - Dragoș Tiselice
-    - Leon Scroggins III
-    - Matt Sarett
-    - Mike Klein
-    - Sami Boukortt
-    - Wan-Teh Chang
- * Loongson Technology Corporation Ltd.
-    - GuXiWei (顾希伟)
-    - JinBo (金波)
-    - ZhangLixia (张利霞)
- * Samsung Group
-    - Filip Wasil
- * SpacemiT Hangzhou Technology, Co.
-    - Liang Junzhao (梁俊钊)
+   - Richard Townsend
+ * Google Inc.
+   - Matt Sarett
+   - Mike Klein
+   - Dan Field
+   - Sami Boukortt
 
 The build projects, the build scripts, the test scripts, and other
-files in the "projects", "scripts" and "tests" directories, have
+files in the "ci", "projects", "scripts" and "tests" directories, have
 other copyright owners, but are released under the libpng license.
 
-Some files in the "ci" and "contrib" directories, as well as some
-of the tools-generated files that are distributed with libpng, have
-other copyright owners, and are released under other open source
-licenses.
+Some files in the "contrib" directory, and some tools-generated files
+that are distributed with libpng, have other copyright owners, and are
+released under other open source licenses.
 ```
-
